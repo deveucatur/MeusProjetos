a535e7cddc2414a3f241b1e99af3664f75a2392a
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-02-02 10:40:17 -0400

    att teste

--- pages/4 - testeVar.py
+++ pages/4 - testeVar.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import os
 
-db_user = os.environ.get('DB_USER')
-db_password = os.environ.get('DB_PASSWORD')
-db_host = os.environ.get('DB_HOST')
-db_port = os.environ.get('DB_PORT')
-db_database = os.environ.get('DB_DATABASE')
+db_user = os.environ.get('"DB_USER"')
+db_password = os.environ.get('"DB_PASSWORD"')
+db_host = os.environ.get('"DB_HOST"')
+db_port = os.environ.get('"DB_PORT"')
+db_database = os.environ.get('"DB_DATABASE"')
 
 st.info(db_user)
 st.info(db_password)
