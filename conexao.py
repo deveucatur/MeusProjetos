24682874de70092e2b6b4618357786658127b0b1
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-02-02 14:43:52 -0400

    Puxando valores do secrets na main

--- conexao.py
+++ conexao.py
@@ -1,5 +1,15 @@
 import os
 import mysql.connector
+import streamlit as st
+
+def variavelYAML():
+    db_user = os.environ.get('DB_USER')
+    db_password = os.environ.get('DB_PASSWORD')
+    db_host = os.environ.get('DB_HOST')
+    db_port = os.environ.get('DB_PORT')
+    db_database = os.environ.get('DB_DATABASE')
+
+    return [db_user, db_password, db_host, db_port, db_database]
 
 def conexaoBD():
     conexao = mysql.connector.connect(
--- pages/4 - testeVar.py
+++ pages/4 - testeVar.py
@@ -1,15 +1,5 @@
-import streamlit as st
-import os
+from conexao import variavelYAML
 
-db_user = os.environ.get('$DB_USER')
-db_password = os.environ.get('$DB_PASSWORD')
-db_host = os.environ.get('$DB_HOST')
-db_port = os.environ.get('$DB_PORT')
-db_database = os.environ.get('$DB_DATABASE')
+connect = variavelYAML()
 
-st.write("teste - 3.0")
-st.info(db_user)
-st.info(db_password)
-st.info(db_host)
-st.info(db_port)
-st.info(db_database)
+st.write(connect)
