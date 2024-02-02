f555e953c7c1c18111bdf9d7112be8c98a59c04c
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-02-02 13:59:13 -0400

    Lendo variavel do yaml

--- pages/4 - testeVar.py
+++ pages/4 - testeVar.py
@@ -1,15 +1,29 @@
 import streamlit as st
 import os
+import yaml
 
-db_user = os.environ.get('$DB_USER')
-db_password = os.environ.get('$DB_PASSWORD')
-db_host = os.environ.get('$DB_HOST')
-db_port = os.environ.get('$DB_PORT')
-db_database = os.environ.get('$DB_DATABASE')
+def ler_configuracoes(nome_arquivo):
+    with open(nome_arquivo, 'r') as arquivo:
+        configuracoes = yaml.safe_load(arquivo)
+    return configuracoes
 
-st.write("teste - 3.0")
-st.info(db_user)
-st.info(db_password)
-st.info(db_host)
-st.info(db_port)
-st.info(db_database)
+# Caminho do arquivo YAML
+caminho_arquivo_yaml = '.github\workflows\main.yaml'
+
+# Lê as variáveis do arquivo YAML
+config = ler_configuracoes(caminho_arquivo_yaml)
+
+st.write(config)
+
+# db_user = os.environ.get('$DB_USER')
+# db_password = os.environ.get('$DB_PASSWORD')
+# db_host = os.environ.get('$DB_HOST')
+# db_port = os.environ.get('$DB_PORT')
+# db_database = os.environ.get('$DB_DATABASE')
+
+# st.write("teste - 3.0")
+# st.info(db_user)
+# st.info(db_password)
+# st.info(db_host)
+# st.info(db_port)
+# st.info(db_database)
