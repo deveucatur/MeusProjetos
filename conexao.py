39f5c7f4666942f0509f850aced9ea71a426f445
Rahyan Ramos <rahyanramos.dev@gmail.com>
2024-02-02 10:02:03 -0400

    Return conexao

--- conexao.py
+++ conexao.py
@@ -2,18 +2,12 @@ import os
 import mysql.connector
 
 def conexaoBD():
-    db_user = os.environ.get('DB_USER')
-    db_password = os.environ.get('DB_PASSWORD')
-    db_host = os.environ.get('DB_HOST')
-    db_port = os.environ.get('DB_PORT')
-    db_database = os.environ.get('DB_DATABASE')
-
     conexao = mysql.connector.connect(
-        user=db_user,
-        passwd=db_password,
-        host=db_host,
-        port=db_port,
-        database=db_database
+        passwd='nineboxeucatur',
+        port=3306,
+        user='ninebox',
+        host='nineboxeucatur.c7rugjkck183.sa-east-1.rds.amazonaws.com',
+        database='projeu'
     )
 
     return conexao
